018bfd36c41e589c6d7c05f922d52931f7eda961
Ruslan <ruslan.kret@yandex.ru>
2023-03-19 11:42:30 +0300

    js completed

--- js/script.js
+++ js/script.js
@@ -1,36 +1,56 @@
-const inputNumber = document.querySelector('#number');
-const inputDate = document.querySelector('#date')
-
-
-
-inputNumber.oninput = () => {
-    const mask = {
-        mask: '0000 0000 0000 0000'
-    };
-
-    if (inputNumber.value) {
-        IMask(inputNumber, mask);
-        document.querySelector('.card__number').innerHTML = inputNumber.value;
-    } else {
-        document.querySelector('.card__number').innerHTML = mask.mask;
-    }
-
-};
-
-inputDate.oninput = () => {
-    const mask = {
-        mask: '00/00'
-    }
-
-    if (inputDate.value) {
-        if (inputDate.value / 1000 <= 12 && inputDate.value % 1000 <= 12) {
-            IMask(inputDate, mask);
-            document.querySelector('.card__date').innerHTML = inputDate.value;
-        } else {
-            inputDate.value = '';
+function formatInputData(input, mask, output) {
+    input.oninput = () => {
+        if (mask) {
+            output.innerHTML = input.value;
         }
 
-    } else {
-        document.querySelector('.card__date').innerHTML = mask.mask;
-    }
-};
+        if (input.value) {
+            IMask(input, mask);
+            output.innerHTML = input.value;
+        } else {
+            output.innerHTML = mask.mask;
+        }
+    };
+}
+
+// card name
+formatInputData(
+    document.querySelector('#name'),
+    mask = {},
+    document.querySelector('.card__name')
+);
+
+// card number
+formatInputData(
+    document.querySelector('#number'),
+    mask = { mask: '0000 0000 0000 0000' },
+    document.querySelector('.card__number')
+);
+
+// card date
+formatInputData(
+    document.querySelector('#date'),
+    mask = {
+        mask: 'MM/YY',
+        // create mask blocks
+        blocks: {
+
+            YY: {
+                mask: '00'
+            },
+            MM: {
+                mask: IMask.MaskedRange,
+                from: 1,
+                to: 12
+            }
+        }
+    },
+    document.querySelector('.card__date')
+);
+
+// card cvc
+formatInputData(
+    document.querySelector('#cvc'),
+    mask = { mask: '000' },
+    document.querySelector('.card__cvc')
+);
